55cd0c82774546bef022b22283cb42afa596cff4
Igor Spiridonov <ispiridonov@27co.de>
2025-06-26 19:56:36 +0300

    Release 0.0.14

--- flutter/android/build.gradle.kts
+++ flutter/android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
 dependencies {
     implementation("androidx.annotation:annotation:1.9.1")
-    implementation("org.uvccamera:lib:$version") // NOTE: Use org.uvccamera:lib of the same version as the plugin.
+    implementation("org.uvccamera:lib:0.0.9") // NOTE: Use org.uvccamera:lib of the same version as the plugin.
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.0.0")
 }
